82fcb6151f0cd2d7ce26086b0db29661118b5d7a
Julecko <julecko@dilino.sk>
2025-03-26 18:39:42 +0100

    Recent changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+const ASSET_URL = process.env.ASSET_URL || '';
+
 export default defineConfig({
   plugins: [react()],
+  base: `${ASSET_URL}/`,
   server: {
     proxy: {
       '/api': {
